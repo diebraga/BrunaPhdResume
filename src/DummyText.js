1e564738f91b987c8d1313eb87536dee87c0ab33
unknown <diebraga0055@gmail.com>
2020-11-17 23:41:58 +0000

    hello

---
+++ src/DummyText.js
@@ -0,0 +1,9 @@
+const dummyText = `Welcome											
+This portfolio provides evidence of the development of my work as an educator and researcher, my professional development and intellectual growth, as well as the demonstration of collaborative and independent production within the areas of Education, Psychology & Digital Technology. It shows a particular blend of experiences, qualifications and achievements that makes me a strong candidate for the PhD programme in Learning Sciences at the Werklund School of Education, University of Calgary.
+
+In this document, I will list the work I have previously executed, content I have produced, and achievements I have accumulated. Further to that, I also hope to introduce myself as both a person and a professional, by elaborating upon each meaningful step of my career which has brought me to where I am today. Additionally, I will highlight how my career so far is motivating me to go forward.
+
+To navigate, just scroll down or click on the specific sections on the left of the page.
+.`;
+
+export default dummyText;
